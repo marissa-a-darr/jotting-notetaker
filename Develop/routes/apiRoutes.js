613c71a6d94a02d626083a86c36dcd99d9bfcdfc
Marissa Darr <mrsaandee@gmail.com>
2023-03-05 00:40:53 -0500

    added working route for saving, editing, deleting

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,13 +1,53 @@
-const api = require ('express').Router();
+const router = require ('express').Router();
 const fs = require ('fs');
 const database = require ('../db/db.json')
 const { v4: uuidv4 } = require("uuid");
+const { writeToFile, readAndAppend, readFromFile } = require('../helpers/fsUtils');
 
-api.get('/notes', (req, res) => {
-  database =JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
-  res.json(database);
-});
 
-api.post('/notes', (req, res) => {
-  id:uuidv4().slice
+router.get('/notes', (req, res) => {
+  // db = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+  // res.json(db)
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 })
+
+router.post('/notes', (req, res) => {
+ 
+  const {title, text} = req.body;
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4()
+    };
+
+    console.log(newNote)
+    
+    readAndAppend(newNote, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response)
+  } else {
+    res.json('Note was unable to save')
+  }
+})
+
+
+router.delete('/notes/:id', (req, res) => {
+  const noteId= req.params.id;
+  readFromFile('./db/db.json')
+  .then((data) => JSON.parse(data))
+  .then((json) => {
+    const result = json.filter((note) => note.id !== noteId)
+    writeToFile('./db/db.json', result);
+    console.log(result)
+    res.json(`Note ${noteId} has been deleted`);
+  });
+})
+
+
+module.exports = router
--- README.md
+++ README.md
@@ -1,21 +1,24 @@
-# Jotting Notetaker 
+# Jotting Notetaker
 
 ## Table of Contents
+* [License](#license)  
 * [Description](#description)
-* [Installation](#installation)
-
+* [Screenshot](#screenshot) 
+* [Questions](#questions)
 
 ## Description <a name="description"></a>   
-User can enter new notes and save them for later reference
+Jotting Notetaker is an app designed for the user to write and save notes! The user may delete, save, view, or edit past notes. 
 
-## Screenshot 
+## License <a name="license"></a>  
+MIT License [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+* https://choosealicense.com/licenses/mit/
 
-## Installation <a name="installation"></a>  
-Run ```npm i```
+## Screenshot <a name="screenshot"></a>  
+![Jotting Notetaker Screenshot](./Develop/public/assets/images/notetakerscreenshot.png)
 
 
 
-
-
+## Questions <a name="questions"></a>  
 * [Github](https://github.com/marissa-a-darr)
 * Email: mrsaanadee@gmail.com
+* Other Contact: Please send me a message via email 
